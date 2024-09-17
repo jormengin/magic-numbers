37442fa248f2b4ca4ef87678ad90939b47c2f126
Jorge Mendez <jorge.mendezgines@gmail.com>
2024-09-17 22:42:02 +0200

    disables multiplayer

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,16 +1,13 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
+// const firebaseConfig = {
 
-};
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-console.log(app);
-const db = getFirestore(app);
-console.log(db);
+// const app = initializeApp(firebaseConfig);
+// console.log(app);
+// const db = getFirestore(app);
+// console.log(db);
 
-export { db };
+// export { db };
